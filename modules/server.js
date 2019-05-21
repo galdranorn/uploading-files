69f45c1f513b6ebf1a8f89ea6b2d0edcfc5d923f
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-05-21 22:34:02 +0200

    Modify port to process.env.PORT in server.js

--- modules/server.js
+++ modules/server.js
@@ -26,7 +26,7 @@ function start() {
     }
   }
 
-  http.createServer(onRequest).listen(9000);
+  http.createServer(onRequest).listen(process.env.PORT || 9000);
 
   console.log("Uruchomiono serwer!".green);
 }
